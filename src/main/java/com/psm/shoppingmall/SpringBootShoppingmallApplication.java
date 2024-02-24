ac4c0a631a598dcd4ae0ba40ad39dff1946c3e4c
psm4171 <kkjjhh4171@naver.com>
2024-02-24 16:00:17 +0900

    build.gradle

---
+++ src/main/java/com/psm/shoppingmall/SpringBootShoppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.psm.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShoppingmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShoppingmallApplication.class, args);
+	}
+
+}
