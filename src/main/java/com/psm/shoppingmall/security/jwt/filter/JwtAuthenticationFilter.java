0c2cf07be51ad79bbb0b08f78204d0fdfaae2ce1
psm4171 <kkjjhh4171@naver.com>
2024-02-27 13:29:24 +0900

    feat: JwtAuthenticationFilter.java

---
+++ src/main/java/com/psm/shoppingmall/security/jwt/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.psm.shoppingmall.security.jwt.filter;
+
+import com.psm.shoppingmall.security.jwt.exception.JwtExceptionCode;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+
+        String token = "";
+
+        try{
+            token = getToken(request);
+            if(StringUtils.hasText(token)){
+                getAuthentication(token);
+            }
+            filterChain.doFilter(request, response);
+        }catch (NullPointerException | IllegalStateException e){
+            request.setAttribute("exception", JwtExceptionCode.INVALID_TOKEN);
+            log.error("Not found Token", token);
+        }
+
+    }
+
+    private String getToken(HttpServletRequest request){
+        String authorization = request.getHeader("Authorization");
+        if(StringUtils.hasText(authorization) && authorization.startsWith("Bearer")){
+            String[] arr = authorization.split(" ");
+            return  arr[1];
+        }
+        return null;
+    }
+
+    private String getAuthentication(String token){
+        JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(token);
+        authenticationManager.authenticate(authenticationToken);
+        SecurityContextHolder.getContext()
+            .setAuthentication(authenticationToken);
+    }
+}
