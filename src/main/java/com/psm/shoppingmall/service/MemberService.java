c5749c86cf31655078b49ef8e6c1584dce25282d
psm4171 <kkjjhh4171@naver.com>
2024-03-01 23:24:39 +0900

    modify: MemberService.java - addMember

--- src/main/java/com/psm/shoppingmall/service/MemberService.java
+++ src/main/java/com/psm/shoppingmall/service/MemberService.java
@@ -3,6 +3,7 @@ package com.psm.shoppingmall.service;
 import com.psm.shoppingmall.domain.Member;
 import com.psm.shoppingmall.domain.Role;
 import com.psm.shoppingmall.repository.MemberRepository;
+import com.psm.shoppingmall.repository.RoleRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
-   // private final
+    private final RoleRepository roleRepository;
 
     @Transactional(readOnly = true)
     public Member findByEmail(String email){
         return memberRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
+    }
+
+    @Transactional
+    public Member addMember(Member member){
+        Optional<Role> userRole = roleRepository.findByName("ROLE_USER");
+        member.addRole(userRole.get());
+        Member saveMember = memberRepository.save(member);
+        return saveMember;
     }
 
     @Transactional(readOnly = true)
