2c8d83abc9a9908a29bbeecf461ab2e7ca6b1723
psm4171 <kkjjhh4171@naver.com>
2024-03-06 18:29:54 +0900

    modify: SpringBootShoppingmallApplication.java

--- src/main/java/com/psm/shoppingmall/SpringBootShoppingmallApplication.java
+++ src/main/java/com/psm/shoppingmall/SpringBootShoppingmallApplication.java
@@ -2,12 +2,16 @@ package com.psm.shoppingmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = {"com.psm.shoppingmall.repository"})
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SpringBootShoppingmallApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootShoppingmallApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootShoppingmallApplication.class, args);
+    }
 
 }
